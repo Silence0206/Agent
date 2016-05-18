058dfd368d1249e62685ab332ea632b4a3104bb4
Silence0206 <732379020@qq.com>
2016-05-18 19:48:23 +0800

    22

--- src/jadetest/Demo1.java
+++ src/jadetest/Demo1.java
@@ -6,18 +6,25 @@ import java.sql.SQLException;
 public class Demo1 {
 
 	static String sql = null;
+	static String sql1 = null;
 	static DBHelper db1 = null;
 	static ResultSet ret = null;
 	static int result=0;
 
 	public static void main(String[] args) {
-		sql = " insert into studenttype (StudentTypeId,StudentTypeName) values(7,'S')";//SQL语句
-		db1 = new DBHelper(sql);//创建DBHelper对象
+		//sql = " insert into studenttype (StudentTypeId,StudentTypeName) values(7,'S')";//SQL语句
+		sql1="CREATE TABLE REGISTRATION1 " +
+                "(id INTEGER not NULL, " +
+                " first VARCHAR(255), " + 
+                " last VARCHAR(255), " + 
+                " age INTEGER, " + 
+                " PRIMARY KEY ( id ))"; 
+		db1 = new DBHelper(sql1);//创建DBHelper对象
 
 		try {
 
 			result = db1.pst.executeUpdate();//执行语句，得到结果集
-			
+			System.out.println(result);
 			db1.close();//关闭连接
 		} catch (SQLException e) {
 			e.printStackTrace();
